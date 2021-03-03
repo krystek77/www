23db966a1dd575d105bf60732197e8dfc2996a1a
krystek77 <krystian.wrona77@gmail.com>
2021-03-03 12:49:31 +0100

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+function App() {
+  return (
+    <Fragment>
+      <h1>Header H1</h1>
+      <h2>Header H2</h2>
+      <h3>Header H3</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sit
+        expedita unde et quia aperiam. Temporibus dolorem totam, dolorum maxime
+        facere, eligendi quas, repellendus atque sint nam incidunt
+        exercitationem neque!
+      </p>
+      <a href='/'>Link 1</a>
+      <a href='/'>Link 2</a>
+    </Fragment>
+  );
+}
+export default App;
