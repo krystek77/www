7deb04e94e00427487d64964b93f50270b86e19b
krystek77 <krystian.wrona77@gmail.com>
2021-03-05 14:21:01 +0100

    Change logo image

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -9,7 +9,7 @@ function Toolbar() {
       <Link to='/' className='logo' onClick={closeSubmenu}>
         <img
           className='logo__image'
-          src='../assets/images/logo.svg'
+          src='../assets/images/logo_test.svg'
           alt='logo'
           width='80'
           height='50'
