ba9ea8784effcbf186fa21dfea6f1cadb8a2239e
krystek77 <krystian.wrona77@gmail.com>
2021-03-11 13:57:15 +0100

    Replace text bay icons

--- client/src/components/Authmenu.js
+++ client/src/components/Authmenu.js
@@ -1,4 +1,5 @@
 import { NavLink as Link } from 'react-router-dom';
+import { FiLogOut, FiLogIn } from 'react-icons/fi';
 import { useNavigationContext } from '../contexts/navigation';
 function Authmenu({ sidebar }) {
   const { closeSidebar, closeSubmenu } = useNavigationContext();
@@ -10,12 +11,12 @@ function Authmenu({ sidebar }) {
     <ul className={`auth-menu ${sidebar && 'auth-menu--sidebar'}`}>
       <li className='auth-menu__item'>
         <Link className='auth-menu__link' to='/logout' onClick={close}>
-          log out
+          <FiLogOut />
         </Link>
       </li>
       <li className='auth-menu__item'>
         <Link className='auth-menu__link' to='/login' onClick={close}>
-          log in
+          <FiLogIn />
         </Link>
       </li>
       <li className='auth-menu__item'>
