224a14cee52ca40e8dc947dba74ee6ae9713f7c5
krystek77 <krystian.wrona77@gmail.com>
2021-03-12 11:21:23 +0100

    Change styles of sidebar

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components';
 import { NavigationContextProvider } from './contexts/navigation';
-// import './sass/index.scss';
-import './sass/global.scss';
+import './sass/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
